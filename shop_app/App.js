f792a9ba3f01499b6263e49031bbf2d6220151d7
TicoUser <thaigo.trabalho611@gmail.com>
2024-11-06 12:45:55 -0300

    Adicionando elementos

--- shop_app/App.js
+++ shop_app/App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'; import { NavigationContainer } from '@react-navigation/native'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import ReposicaoScreen from './src/screens/ReposicaoScreen'; 
+import EstoqueScreen from './src/screens/EstoqueScreen'; import FluxoCaixaScreen from './src/screens/FluxoCaixaScreen'; 
+import ControleGeralScreen from './src/screens/ControleGeralScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const Tab = createBottomTabNavigator(); 
+function MyTabs() { 
+  return ( 
+    <Tab.Navigator> 
+      <Tab.Screen name="Reposição" component={ReposicaoScreen} /> 
+      <Tab.Screen name="Estoque" component={EstoqueScreen} /> 
+      <Tab.Screen name="Fluxo de Caixa" component={FluxoCaixaScreen} /> 
+      <Tab.Screen name="Controle Geral" component={ControleGeralScreen} /> 
+    </Tab.Navigator> 
+  ); 
+} 
+export default function App() { 
+  return ( 
+    <NavigationContainer> 
+      <MyTabs /> 
+    </NavigationContainer> 
   );
-}
+}  
 
 const styles = StyleSheet.create({
   container: {
